0eb951b65ac218711b352e15d1f94482d89e2853
javierph389 <javierperezherrera@gmail.com>
2022-09-03 19:02:02 -0400

    Se agrega interaccion con boton Enter

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-document.querySelector('#push').onclick = function () {
+function enter(e){
+    if(e.keyCode==13){
+        addtask();
+    }
+}
+
+let addtask = document.querySelector('#push').onclick = function () { //se declara funcion que agregara elementos a la lista
     if (document.querySelector('#ingresoTarea').value.length == 0) {
         alert('Por favor, agrega una tarea')
     }
@@ -13,7 +19,7 @@ document.querySelector('#push').onclick = function () {
         </button>
     </div>
 `;
-        let tareasActuales = document.querySelectorAll('.borrar'); //declarar variable que borra variables
+        let tareasActuales = document.querySelectorAll('.borrar'); //declarar variable que borra tareas
         for (let i = 0; i < tareasActuales.length; i++) {
             tareasActuales[i].onclick = function () {
                 this.parentNode.remove();
